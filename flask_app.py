0702e581c6aa70a9f5580ebe9e93d66a13457360
naam <reiniervanderwesten@hotmail.com>
2023-10-12 19:52:56 +0200

    basic Flask Code

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<p> Happy Halloween! </p>"
+    return "<p> Hello World! </p>"
 
 if __name__=='__main__':
     app.run(port=5000, debug=True)
